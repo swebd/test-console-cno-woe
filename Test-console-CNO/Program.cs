26ce9f65642afe490940c157d7862c89909e4e4a
Willekens <swebd@hotmail.com>
2025-03-19 14:40:12 +0100

    Add files via upload

---
+++ Test-console-CNO/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_console_CNO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * NAAM: 
+            */
+
+
+        }
+    }
+}
